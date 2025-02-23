a0cd6c1d1c33f4b210c3798f251eec4b6da380ab
Dope-Otaku <fakeyt909@gmail.com>
2025-02-23 15:52:51 +0530

    added an incorrect usage output

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,15 @@
 #include <iostream>
 
 int main(int argc, char*argv[]){
-    std::cout << argv[0] << std::endl;
-    return 0;
+
+    if (argc!=2){
+        std::cerr << "Incorrect Usage, Correct usage is..." << std::endl;
+        std::cerr << "com <input.co>" << std::endl;
+        return EXIT_FAILURE;
+    }
+
+
+
+    std::cout << argv[1] << std::endl;
+    return EXIT_SUCCESS;
 }
